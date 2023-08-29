9e9582d210f72d54b1a6d18686da56915cf6b0ae
fmichal2022 <fmichal2022@44SE34-2TV6YW2.ad.campus-eni.fr>
2023-08-29 11:44:23 +0200

    add RetraitsDAO to DAOFACT

--- src/main/java/fr/eni/projetEni/dal/DAOFact.java
+++ src/main/java/fr/eni/projetEni/dal/DAOFact.java
@@ -4,8 +4,13 @@ public class DAOFact {
 	public static ArticleVendusDAO getArticleVenduDAO() {
 		return new ArticleVendusDAOImpl();
 	}
-	
+
 	public static UtilisateursDAO getUtilisateursDAO() {
 		return new UtilisateursDAOImpl();
 	}
+
+	public static RetraitsDAO getRetraitsDAO() {
+		return new RetraitsDAOImpl();
+	}
+	
 }
