5a6bd1e1f152ae9b4ae0eb575be2243707cde1fa
fmichal2022 <fmichal2022@44SE34-2TV6YW2.ad.campus-eni.fr>
2023-08-29 16:19:30 +0200

    init servlet

---
+++ src/main/java/fr/eni/projetEni/ihm/EncheresServlet.java
@@ -0,0 +1,33 @@
+package fr.eni.projetEni.ihm;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/EncheresServlet")
+public class EncheresServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @throws jakarta.servlet.ServletException 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, jakarta.servlet.ServletException {
+		request.getRequestDispatcher("/WEB-INF/encheresConnection.jsp").forward(request, response);
+	}
+
+	/**
+	 * @throws ServletException 
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		request.getRequestDispatcher("/WEB-INF/encheresConnection.jsp").forward(request, response);
+		
+	}
+}
