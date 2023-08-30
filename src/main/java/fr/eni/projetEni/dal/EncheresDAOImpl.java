49cc55ebc41461b0dfbb0346e8bd60ff31430fa5
floma <flomarois@gmail.com>
2023-08-30 16:08:51 +0200

    Réparation enchèreDAOImpl ?

--- src/main/java/fr/eni/projetEni/dal/EncheresDAOImpl.java
+++ src/main/java/fr/eni/projetEni/dal/EncheresDAOImpl.java
@@ -13,22 +13,18 @@ import fr.eni.projetEni.bo.Encheres;
 import fr.eni.projetEni.utils.ConnectionProvider;
 
 public class EncheresDAOImpl implements EncheresDAO {
-
-	final String SELECT = "SELECT * FROM ENCHERES;";
-	final String SELECT_BY_NO_ARTICLE = "SELECT no_enchere,"
-			+ "          e.no_article, a.nom_article, a.description, a.date_debut_encheres, a.date_fin_encheres, a.prix_initial, a.prix_vente, a.etat_vente,"
-			+ "          e.no_utilisateur, u.pseudo, u.nom, u.prenom, u.email, u.telephone, u.rue, u.code_postal, u.ville, u.mot_de_passe, u.credit, u.administrateur,"
-			+ "          no_categorie," + "          date_enchere, montant_enchere"
-			+ "              FROM ENCHERES AS e INNER JOIN UTILISATEURS AS u ON e.no_utilisateur=u.no_utilisateur"
-			+ "                                 INNER JOIN ARTICLES_VENDUS as a on e.no_article = a.no_article"
-			+ "                    where e.no_article=?;";
-	final String INSERT = "INSERT INTO ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere) values (?, ?, ?, ?);";
-	final String SELECT_BY_NO_ENCHERE = "SELECT * FROM ENCHERES WHERE no_enchere = ?";
-	final String SELECT_BY_NO_USER = "SELECT * FROM ENCHERES WHERE no_utilisateur = ?";
-	final String DELETE_BY_NO ="DELETE FROM ENCHERES WHERE no_enchere = ?;";
+	final String INSERT					= """
+			INSERT INTO ENCHERES	(no_utilisateur, no_article, date_enchere, montant_enchere)
+						VALUES		(?			   , ?		   , ?			 , ?);
+			""";
+	final String DELETE					= "DELETE FROM ENCHERES WHERE no_enchere = ?;";
+	final String SELECT_ALL				= "SELECT * FROM ENCHERES;";
+	final String SELECT_BY_NO_ARTICLE	= "SELECT * FROM ENCHERES WHERE no_article=?;";
+	final String SELECT_BY_NO_USER		= "SELECT * FROM ENCHERES WHERE no_utilisateur = ?";
+	final String SELECT_BY_NO_ENCHERE	= "SELECT * FROM ENCHERES WHERE no_enchere = ?";
 
 	@Override
-	public void insert(Encheres encheres) {
+	public void insert(Encheres encheres) throws DalException {
 		try (Connection con = ConnectionProvider.getConnection()){
 			PreparedStatement stmt = con.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
 			stmt.setInt(1, encheres.getNo_utilisateur() );
@@ -39,7 +35,7 @@ public class EncheresDAOImpl implements EncheresDAO {
 			stmt.setDate(3, sqlDate);
 			
 			stmt.setInt(4, encheres.getMontant_enchere());
-		
+			
 			int nb = stmt.executeUpdate();
 			if(nb>0) {
 				ResultSet rs = stmt.getGeneratedKeys();
@@ -48,34 +44,49 @@ public class EncheresDAOImpl implements EncheresDAO {
 				}
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new DalException(e.getMessage());
 		}
 	}
 	
 	@Override
-	public List<Encheres> getAll() {
+	public void delete(int id) throws DalException {
+		try(Connection con = ConnectionProvider.getConnection()){
+			PreparedStatement stmt = con.prepareStatement(DELETE);
+			stmt.setInt(1, id);
+			stmt.executeQuery();
+		}catch (SQLException e) {
+			e.printStackTrace();
+			throw new DalException(e.getMessage());
+		}
+	}
+	
+	@Override
+	public List<Encheres> getAll() throws DalException {
 		List<Encheres> result = new ArrayList<>();
+		
 		try (Connection con = ConnectionProvider.getConnection()) {
-			PreparedStatement stmt = con.prepareStatement(SELECT);
+			PreparedStatement stmt = con.prepareStatement(SELECT_ALL);
 			ResultSet rs = stmt.executeQuery();
 			while (rs.next()) {
 				java.sql.Timestamp timestamp = rs.getTimestamp("date_enchere");
 				LocalDateTime localDateTime = timestamp.toLocalDateTime();
 				Encheres encheres = new Encheres(rs.getInt("no_utilisateur"),
 						rs.getInt("no_article"), localDateTime, rs.getInt("montant_enchere"));
+				encheres.setNo_enchere(rs.getInt("no_enchere"));
 				result.add(encheres);
 			}
 		} catch (SQLException e) {
-//			throw new DALException("ms_insert");
 			e.printStackTrace();
+			throw new DalException(e.getMessage());
 		}
+		
 		return result;
 	}
 
 	@Override                          
-	public Encheres findEnchereByArticleId(int id) {
-		Encheres result = new Encheres();
+	public List<Encheres> findEnchereByArticleId(int id) throws DalException {
+		List<Encheres> result = new ArrayList<>();
 
 		try (Connection con = ConnectionProvider.getConnection()) {
 			PreparedStatement stmt = con.prepareStatement(SELECT_BY_NO_ARTICLE);
@@ -85,18 +96,21 @@ public class EncheresDAOImpl implements EncheresDAO {
 			while (rs.next()) {
 				java.sql.Timestamp timestamp = rs.getTimestamp("date_enchere");
 				LocalDateTime localDateTime = timestamp.toLocalDateTime();
-				result = new Encheres(rs.getInt("no_utilisateur"), rs.getInt("no_article"),
-						localDateTime, rs.getInt("montant_enchere"));
+				Encheres encheres = new Encheres(rs.getInt("no_utilisateur"),
+						rs.getInt("no_article"), localDateTime, rs.getInt("montant_enchere"));
+				encheres.setNo_enchere(rs.getInt("no_enchere"));
+				result.add(encheres);
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new DalException(e.getMessage());
 		}
+		
 		return result;
 	}
 	
 	@Override                          
-	public Encheres findEnchereById(int id) {
+	public Encheres findEnchereById(int id) throws DalException {
 		Encheres result = new Encheres();
 
 		try (Connection con = ConnectionProvider.getConnection()) {
@@ -107,31 +121,21 @@ public class EncheresDAOImpl implements EncheresDAO {
 			while (rs.next()) {
 				java.sql.Timestamp timestamp = rs.getTimestamp("date_enchere");
 				LocalDateTime localDateTime = timestamp.toLocalDateTime();
-				result = new Encheres(rs.getInt("no_utilisateur"), rs.getInt("no_article"),
-						localDateTime, rs.getInt("montant_enchere"));
+				Encheres encheres = new Encheres(rs.getInt("no_utilisateur"),
+						rs.getInt("no_article"), localDateTime, rs.getInt("montant_enchere"));
+				encheres.setNo_enchere(rs.getInt("no_enchere"));
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new DalException(e.getMessage());
 		}
+		
 		return result;
 	}
 	
-	@Override
-	public void deleteEnchereById(int id) {
-		try(Connection con = ConnectionProvider.getConnection()){
-			PreparedStatement stmt = con.prepareStatement(DELETE_BY_NO);
-			stmt.setInt(1, id);
-			stmt.executeQuery();
-		}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 	@Override                          
-	public Encheres findEnchereByUserId(int id) {
-		Encheres result = new Encheres();
+	public List<Encheres> findEnchereByUserId(int id) throws DalException {
+		List<Encheres> result = new ArrayList<>();
 
 		try (Connection con = ConnectionProvider.getConnection()) {
 			PreparedStatement stmt = con.prepareStatement(SELECT_BY_NO_USER);
@@ -141,15 +145,16 @@ public class EncheresDAOImpl implements EncheresDAO {
 			while (rs.next()) {
 				java.sql.Timestamp timestamp = rs.getTimestamp("date_enchere");
 				LocalDateTime localDateTime = timestamp.toLocalDateTime();
-				result = new Encheres(rs.getInt("no_utilisateur"), rs.getInt("no_article"),
-						localDateTime, rs.getInt("montant_enchere"));
+				Encheres encheres = new Encheres(rs.getInt("no_utilisateur"),
+						rs.getInt("no_article"), localDateTime, rs.getInt("montant_enchere"));
+				encheres.setNo_enchere(rs.getInt("no_enchere"));
+				result.add(encheres);
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new DalException(e.getMessage());
 		}
+		
 		return result;
 	}
-	
-
 }
