90c76eea647b2df02b5913b702e537d4ab72a43d
floma <flomarois@gmail.com>
2023-08-30 11:51:53 +0200

    BLL Retraits

--- src/main/java/fr/eni/projetEni/bll/RetraisManagerImpl.java
+++ src/main/java/fr/eni/projetEni/bll/RetraisManagerImpl.java
@@ -1,11 +1,61 @@
 package fr.eni.projetEni.bll;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.projetEni.bo.Retraits;
 import fr.eni.projetEni.dal.DAOFact;
+import fr.eni.projetEni.dal.DalException;
 import fr.eni.projetEni.dal.RetraitsDAO;
-import fr.eni.projetEni.dal.UtilisateursDAO;
 
 public class RetraisManagerImpl implements RetraisManager {
 	private RetraitsDAO dao = DAOFact.getRetraitsDAO();
 	
-	
+	@Override
+	public List<Retraits> getAllRetraits() throws ManagerException {
+		List<Retraits> lst = new ArrayList<Retraits>();
+		
+		try {
+			lst = dao.getAll();
+		} catch (DalException e) {
+			e.printStackTrace();
+			throw new ManagerException(e.getMessage());
+		}
+		
+		return lst;
+	}
+
+	@Override
+	public Retraits getRetraitsByArticleNo(int id) throws ManagerException {
+		Retraits result = null;
+		
+		try {
+			result = dao.findRetraitsByNoArticle(id);
+		} catch (DalException e) {
+			e.printStackTrace();
+			throw new ManagerException(e.getMessage());
+		}
+		
+		return result;
+	}
+
+	@Override
+	public void addRetraits(Retraits retrait) throws ManagerException {
+		try {
+			dao.insert(retrait);
+		} catch (DalException e) {
+			e.printStackTrace();
+			throw new ManagerException(e.getMessage());
+		}
+	}
+
+	@Override
+	public void supprimerRetraits(int id) throws ManagerException {
+		try {
+			dao.delete(id);
+		} catch (DalException e) {
+			e.printStackTrace();
+			throw new ManagerException(e.getMessage());
+		}
+	}
 }
