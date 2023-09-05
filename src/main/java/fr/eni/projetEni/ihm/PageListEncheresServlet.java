09380d332854b9e5a5bfa779fdbb49ec540bd31b
fmichal2022 <fmichal2022@44SE34-2TV6YW2.ad.campus-eni.fr>
2023-09-05 14:51:43 +0200

    quipush

--- src/main/java/fr/eni/projetEni/ihm/PageListEncheresServlet.java
+++ src/main/java/fr/eni/projetEni/ihm/PageListEncheresServlet.java
@@ -2,9 +2,8 @@ package fr.eni.projetEni.ihm;
 
 import java.io.IOException;
 
-import fr.eni.projetEni.bll.ManagerException;
-import fr.eni.projetEni.bll.UtilisateurManager;
-import fr.eni.projetEni.bll.UtilisateurManagerSing;
+import fr.eni.projetEni.bll.EnchereManager;
+import fr.eni.projetEni.bll2.EnchereManagerSing;
 import fr.eni.projetEni.bo2.Utilisateur;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -19,7 +18,7 @@ import jakarta.servlet.http.HttpSession;
 @WebServlet("/")
 public class PageListEncheresServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private UtilisateurManager uManager = UtilisateurManagerSing.getInstance();
+	private EnchereManager eManager = (EnchereManager) EnchereManagerSing.getInstance();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -41,7 +40,9 @@ public class PageListEncheresServlet extends HttpServlet {
 			} else {
 				Utilisateur utilisateur = new Utilisateur();
 				utilisateur = (Utilisateur) session.getAttribute("utilisateurConnecte");
-
+				// code generation des enchères connecter
+				
+				
 				request.getRequestDispatcher("/WEB-INF/pageListEncheresConnecte.jsp").forward(request, response);
 			}
 		}
@@ -49,27 +50,27 @@ public class PageListEncheresServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String login = request.getParameter("login");
-		String password = request.getParameter("password");
-		HttpSession session = request.getSession();
-		Utilisateur utilisateur = new Utilisateur();
-		if(request.getParameter("deconnexion").equalsIgnoreCase("deconnexion")) {
-		System.out.println("test:  " + session.getAttribute("utilisateurConnecte").toString());
-			session.invalidate();
-		};
-
-		if (session.getAttribute("utilisateurConnecte") == null) {
-			request.getRequestDispatcher("/WEB-INF/pagesAccueilNonConnecte.jsp").forward(request, response);
-		} else {
-			utilisateur = (Utilisateur) session.getAttribute("utilisateurConnecte");
-			try {
-				utilisateur = uManager.check(login, password);
-			} catch (ManagerException e) {
-				e.printStackTrace();
-			}
-
-			request.getRequestDispatcher("/WEB-INF/pageListEncheresConnecte.jsp").forward(request, response);
-		}
+//		String login = request.getParameter("login");
+//		String password = request.getParameter("password");
+//		HttpSession session = request.getSession();
+//		Utilisateur utilisateur = new Utilisateur();
+//		if(request.getParameter("deconnexion").equalsIgnoreCase("deconnexion")) {
+//		System.out.println("test:  " + session.getAttribute("utilisateurConnecte").toString());
+//			session.invalidate();
+//		};
+//
+//		if (session.getAttribute("utilisateurConnecte") == null) {
+//			request.getRequestDispatcher("/WEB-INF/pagesAccueilNonConnecte.jsp").forward(request, response);
+//		} else {
+//			utilisateur = (Utilisateur) session.getAttribute("utilisateurConnecte");
+//			try {
+//				utilisateur = uManager.check(login, password);
+//			} catch (ManagerException e) {
+//				e.printStackTrace();
+//			}
+//
+//			request.getRequestDispatcher("/WEB-INF/pageListEncheresConnecte.jsp").forward(request, response);
+//		}
 
 	}
 
